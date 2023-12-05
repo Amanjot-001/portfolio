96c33029e606eb9906e66e62bc581620abbb1c9c
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-05 17:03:10 +0530

    react setup

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { ReactDOM } from "react";
+import AppLayout from "./src/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
