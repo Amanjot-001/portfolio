1706e899c99611e0863708ace220c4446131e4bc
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-22 19:24:37 +0530

    .

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppLayout from "./src/App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
