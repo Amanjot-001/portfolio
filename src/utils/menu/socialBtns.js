473500002e02955cf52c4484b038a3ad76faf8fc
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-01-06 22:07:08 +0530

    resume added in social

--- src/utils/menu/socialBtns.js
+++ src/utils/menu/socialBtns.js
@@ -3,28 +3,33 @@ import x from '../../assets/images/socials/x.png'
 import linkedIn from '../../assets/images/socials/linkedin.png'
 import instagram from '../../assets/images/socials/instagram.png'
 import mail from '../../assets/images/socials/email.png'
+import resume from '../../assets/images/socials/resume.png'
 
 const socialData = [
-    {
-        "logo": github,
-        "link": "https://github.com/Amanjot-001"
-    },
-    {
-        "logo": x,
-        "link": "https://x.com/Amanjotsingh001?t=Y8pBfV71mb4zfZ_i-MoxgQ&s=09"
-    },
-    {
-        "logo": linkedIn,
-        "link": "https://www.linkedin.com/in/amanjot-singh-2298a6232/"
-    },
-    {
-        "logo": instagram,
-        "link": "https://www.instagram.com/amanjotsingh_001?igsh=MXVsbWpzb3BycDJhcg=="
-    },
-    {
-        "logo": mail,
-        "link": "mailto:amanjotsingh2309@gmail.com"
-    }
+  {
+    "logo": github,
+    "link": "https://github.com/Amanjot-001"
+  },
+  {
+    "logo": x,
+    "link": "https://x.com/Amanjotsingh001?t=Y8pBfV71mb4zfZ_i-MoxgQ&s=09"
+  },
+  {
+    "logo": linkedIn,
+    "link": "https://www.linkedin.com/in/amanjot-singh-2298a6232/"
+  },
+  {
+    "logo": instagram,
+    "link": "https://www.instagram.com/amanjotsingh_001?igsh=MXVsbWpzb3BycDJhcg=="
+  },
+  {
+    "logo": mail,
+    "link": "mailto:amanjotsingh2309@gmail.com"
+  },
+  {
+    "logo": resume,
+    "link": "https://drive.google.com/file/d/1cjvf66Nrm2UC9IcWVdNjZei2mbJE3m9y/view?usp=sharing"
+  },
 ];
 
 export default socialData;
