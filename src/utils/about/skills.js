462f6a8bb483b1d4abe5ded21724ac617ff7cd93
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-01-07 12:30:11 +0530

    skills util added

---
+++ src/utils/about/skills.js
@@ -0,0 +1,100 @@
+import js from '../../assets/images/skills/js.png'
+import ts from '../../assets/images/skills/ts.png'
+import c from '../../assets/images/skills/c.png'
+import cpp from '../../assets/images/skills/c++.png'
+import python from '../../assets/images/skills/python.png'
+import react from '../../assets/images/skills/react.png'
+import redux from '../../assets/images/skills/redux.png'
+import tailwind from '../../assets/images/skills/tailwind.png'
+import html from '../../assets/images/skills/html.png'
+import css from '../../assets/images/skills/css.png'
+import mongo from '../../assets/images/skills/mongo.png'
+import sql from '../../assets/images/skills/sql.png'
+import node from '../../assets/images/skills/node.png'
+import express from '../../assets/images/skills/express.png'
+import linux from '../../assets/images/skills/linux.png'
+import windows from '../../assets/images/skills/windows.png'
+
+const skills = [
+  {
+    "title": "Languages",
+    "stack": [
+      {
+        "name": "Javascript",
+        "image": js,
+      },
+      {
+        "name": "Typescript",
+        "image": ts,
+      },
+      {
+        "name": "C",
+        "image": c,
+      },
+      {
+        "name": "C++",
+        "image": cpp,
+      },
+      {
+        "name": "Python",
+        "image": python,
+      },
+    ],
+  },
+  {
+    "title": "Web Techs",
+    "stack": [
+      {
+        "name": "React.js",
+        "image": react,
+      },
+      {
+        "name": "Redux",
+        "image": redux,
+      },
+      {
+        "name": "Node.js",
+        "image": node,
+      },
+      {
+        "name": "Express.js",
+        "image": express,
+      },
+      {
+        "name": "Tailwind",
+        "image": tailwind,
+      },
+      {
+        "name": "MongoDb",
+        "image": mongo,
+      },
+      {
+        "name": "Sql",
+        "image": sql,
+      },
+      {
+        "name": "Html",
+        "image": html,
+      },
+      {
+        "name": "Css",
+        "image": css,
+      },
+    ],
+  },
+  {
+    "title": "Operating Systems",
+    "stack": [
+      {
+        "name": "Linux",
+        "image": linux,
+      },
+      {
+        "name": "Windows",
+        "image": windows,
+      },
+    ],
+  },
+];
+
+export default skills;
